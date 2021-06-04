25b583ba903fd285dd79e091ddd7edf5eff914ce
Yusuf Sabbag <sist.heb@gmail.com>
2021-06-04 19:29:55 +0300

    improved logic and changed function names

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // get elements
-function $id(id){
+function $id(id) {
     return document.getElementById(id)
 }
 const rad = $id("radius")
@@ -9,51 +9,50 @@ const crc = $id("circle")
 
 // create our class
 class Circle {
-    #pi = Math.PI
-    constructor(r){
+    constructor(r) {
         this.r = r
         this.pi = Math.PI
     }
-    perimeter(){
-        return +(2 * this.#pi * this.r).toFixed(2)
+    perimeter() {
+        return +(2 * this.pi * this.r).toFixed(2)
     }
-    area(){
-        return +(this.#pi * this.r * this.r).toFixed(2)
+    area() {
+        return +(this.pi * this.r * this.r).toFixed(2)
     }
 }
 
 // create second class
 class DrawCircle extends Circle {
-    constructor(r,cl){
+    constructor(r, cl) {
         super(r)
         this.cl = cl
     }
-    draw(){
+    draw() {
         return [
-        crc.style.width = 2 * this.r +"px",
-        crc.style.height = 2 * this.r +"px",
-        crc.style.backgroundColor = this.cl,
-        crc.style.borderRadius = "50%"]
+            crc.style.width = 2 * this.r + "px",
+            crc.style.height = 2 * this.r + "px",
+            crc.style.backgroundColor = this.cl,
+            crc.style.borderRadius = "50%"
+        ]
     }
 }
 
 // events
 rad.addEventListener("input", () => {
-    doit()
+    drawOnPage()
 })
 clr.addEventListener("input", () => {
-    doit()
+    drawOnPage()
 })
 
 // action
-function doit(){
-    let inp = rad.value
-    let c = new DrawCircle(inp,clr.value)
+function drawOnPage() {
+    let inp = rad.value;
+    let c = new DrawCircle(inp, clr.value);
+    c.draw();
     rez.innerHTML = `Radius : ${c.r}<br />
     Perimeter : ${c.perimeter()}<br />
-    Area : ${c.area()}`
-    c.draw()
+    Area : ${c.area()}`;
+    // distruct to use later if needed 
+    return [w, h, cc, rr] = c.draw()
 }
-
-// distruct to use later if needed
-let [w,h,cc,rr] = c.draw()
